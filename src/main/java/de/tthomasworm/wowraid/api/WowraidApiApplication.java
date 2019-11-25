e43b55a7682883a0889a3e58b3df489b3d00ac64
thomas-worm <thomas.worm@thomas-worm.de>
2019-11-25 06:18:08 +0000

    Added spring skeleton.

---
+++ src/main/java/de/tthomasworm/wowraid/api/WowraidApiApplication.java
@@ -0,0 +1,13 @@
+package de.tthomasworm.wowraid.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowraidApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowraidApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/tthomasworm/wowraid/api/WowraidApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tthomasworm.wowraid.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowraidApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
