c73b651998885588596b1fc87e7bc6e8a5082e5b
thomas-worm <thomas.worm@thomas-worm.de>
2019-11-26 15:37:30 +0000

    Added dummy callback site.

--- src/main/java/de/thomasworm/wowraid/api/auth/battlenet/BattleNetAuthenticationController.java
+++ src/main/java/de/thomasworm/wowraid/api/auth/battlenet/BattleNetAuthenticationController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -25,6 +26,11 @@ class BattleNetAuthenticationController {
     @GetMapping("/oauth2/authorization/battlenet")
     public Mono<Void> authenticate(ServerHttpResponse response, ServerHttpRequest request) {
         return redirect(response, authenticationService.createAuthorizationUri(getRedirectUrl(request)));
+    }
+
+    @GetMapping("/login/oauth2/code/battlenet")
+    public Mono<String> authenticateCallback(@RequestParam("code") String code) {
+        return Mono.just(code);
     }
 
     private Mono<Void> redirect(ServerHttpResponse response, URI uri) {
