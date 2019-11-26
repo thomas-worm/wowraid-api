83de57a6e2f574036e8d5ea4efae64a3ecbe1d0c
thomas-worm <thomas.worm@thomas-worm.de>
2019-11-26 14:08:36 +0000

    Removed OIDC package related function.

--- src/main/java/de/thomasworm/wowraid/api/HelloController.java
+++ src/main/java/de/thomasworm/wowraid/api/HelloController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class HelloController {
 
-    @GetMapping("/me")
+    /** @GetMapping("/me")
     public ResponseEntity<OAuth2AuthenticationToken> hello(OAuth2AuthenticationToken currentUser)  {
         return ResponseEntity.ok(currentUser);
-    }
+    } **/
 
 }
