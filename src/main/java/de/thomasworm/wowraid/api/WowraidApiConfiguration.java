4091dddf3eb9fece6e3998f30e25632105d4015b
thomas-worm <thomas.worm@thomas-worm.de>
2019-11-26 14:32:48 +0000

    Allow all access.

---
+++ src/main/java/de/thomasworm/wowraid/api/WowraidApiConfiguration.java
@@ -0,0 +1,14 @@
+package de.thomasworm.wowraid.api;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+class WowraidApiConfiguration extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .anyRequest()
+            .anonymous();
+    }
+
+}
