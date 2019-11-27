711008d67079e196c00ab0f156e74dba30dfcf54
thomas-worm <thomas.worm@thomas-worm.de>
2019-11-27 18:24:50 +0000

    Added authenticated REST call.

--- src/main/java/de/thomasworm/wowraid/api/HelloController.java
+++ src/main/java/de/thomasworm/wowraid/api/HelloController.java
@@ -1,9 +1,13 @@
 package de.thomasworm.wowraid.api;
 
+import java.security.Principal;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 class HelloController {
@@ -11,6 +15,11 @@ class HelloController {
     @GetMapping("/me")
     public ResponseEntity<OAuth2AuthenticationToken> hello(OAuth2AuthenticationToken currentUser)  {
         return ResponseEntity.ok(currentUser);
+    }
+
+    @GetMapping("/user/authenticated")
+    public Mono<Boolean> isAuthenticated(final Principal principal) {
+        return Mono.just(principal != null);
     }
 
 }
