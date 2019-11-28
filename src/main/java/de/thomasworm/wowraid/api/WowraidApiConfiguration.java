0f7de30b6c6cb98e4bc5fd9c40118f47b45efb8f
thomas-worm <thomas.worm@thomas-worm.de>
2019-11-28 08:07:44 +0000

    Removed samesite from session cookie.

--- src/main/java/de/thomasworm/wowraid/api/WowraidApiConfiguration.java
+++ src/main/java/de/thomasworm/wowraid/api/WowraidApiConfiguration.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.session.web.http.CookieSerializer;
-import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @Configuration()
 class WowraidApiConfiguration {
