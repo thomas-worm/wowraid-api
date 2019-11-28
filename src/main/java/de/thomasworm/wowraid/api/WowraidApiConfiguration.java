9d36ff02655b5b19dea954d77234016cac505ce7
thomas-worm <thomas.worm@thomas-worm.de>
2019-11-28 14:12:35 +0000

    Removed samesite from session cookie.

--- src/main/java/de/thomasworm/wowraid/api/WowraidApiConfiguration.java
+++ src/main/java/de/thomasworm/wowraid/api/WowraidApiConfiguration.java
@@ -2,21 +2,15 @@ package de.thomasworm.wowraid.api;
 
 import java.util.Arrays;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.session.web.http.CookieSerializer;
-import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.server.session.WebSessionIdResolver;
-import org.apache.catalina.Context;
-import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
 
 @Configuration()
 class WowraidApiConfiguration {
@@ -47,30 +41,6 @@ class WowraidApiConfiguration {
         corsConfig.setAllowCredentials(true);
         corsConfigSource.registerCorsConfiguration("/**", corsConfig);
         return corsConfigSource;
-    }
-
-    @Bean()
-    public CookieSerializer cookieSerializer() {
-        return defaultCookieSerializer();
-    }
-
-    @Bean()
-    public DefaultCookieSerializer defaultCookieSerializer() {
-        DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
-        cookieSerializer.setSameSite(null);
-        return cookieSerializer;
-    }
-
-    @Bean()
-    public ServletWebServerFactory servletContainer() {
-        return new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                Rfc6265CookieProcessor rfc6265CookieProcessor = new Rfc6265CookieProcessor();
-                rfc6265CookieProcessor.setSameSiteCookies(null);
-                context.setCookieProcessor(rfc6265CookieProcessor);
-            }
-        };
     }
 
     @Bean()
