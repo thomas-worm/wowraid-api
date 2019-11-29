2a0786d17c48ce6c8f139055e0a54295a5d38ead
thomas-worm <thomas.worm@thomas-worm.de>
2019-11-29 15:21:27 +0000

    Added login endpoint.

--- src/main/java/de/thomasworm/wowraid/api/UserController.java
+++ src/main/java/de/thomasworm/wowraid/api/UserController.java
@@ -3,11 +3,17 @@ package de.thomasworm.wowraid.api;
 import de.thomasworm.wowraid.api.model.UserInfo;
 
 import java.security.Principal;
+import java.util.Arrays;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 
 import reactor.core.publisher.Mono;
 
@@ -22,6 +28,14 @@ class UserController {
     @GetMapping("/user")
     public Mono<UserInfo> getUser(OAuth2AuthenticationToken token) {
         return Mono.just(new UserInfo(token));
+    }
+
+    @GetMapping("/user")
+    public Mono<Void> loginUser(@RequestParam("redirect_uri") String redirectUri, ServerWebExchange exchange) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.FOUND);
+        response.getHeaders().put(HttpHeaders.LOCATION, Arrays.asList(new String[] {redirectUri}));
+        return response.setComplete();
     }
 
     @GetMapping("/user/authenticated")
--- src/main/java/de/thomasworm/wowraid/api/WowraidApiConfiguration.java
+++ src/main/java/de/thomasworm/wowraid/api/WowraidApiConfiguration.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
