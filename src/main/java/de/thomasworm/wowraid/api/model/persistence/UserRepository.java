999dd8ae3eadad93ed92248177f41598a86ca732
thomas-worm <thomas.worm@thomas-worm.de>
2019-12-01 21:34:50 +0000

    Removed heroku installation script and added quotation to database identifiers.

--- src/main/java/de/thomasworm/wowraid/api/model/persistence/UserRepository.java
+++ src/main/java/de/thomasworm/wowraid/api/model/persistence/UserRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
