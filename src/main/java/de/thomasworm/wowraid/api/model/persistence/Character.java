ca5cfc8a142d378c1d706715e82d4277da079a01
thomas-worm <thomas.worm@thomas-worm.de>
2019-12-03 13:28:38 +0000

    Added character model classes.

--- src/main/java/de/thomasworm/wowraid/api/model/persistence/Character.java
+++ src/main/java/de/thomasworm/wowraid/api/model/persistence/Character.java
@@ -1,9 +1,14 @@
 package de.thomasworm.wowraid.api.model.persistence;
 
+import java.util.Optional;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity()
 public class Character {
@@ -12,6 +17,56 @@ public class Character {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    
+    @OneToOne()
+    @JoinColumn()
+    private Optional<User> user;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "class")
+    private CharacterClass characterClass;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn()
+    private CharacterRace race;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn()
+    private Faction faction;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setUser(Optional<User> value) {
+        this.user = value;
+    }
+
+    public Optional<User> getUser() {
+        return this.user;
+    }
+
+    public void setCharacterClass(CharacterClass value) {
+        this.characterClass = value;
+    }
+
+    public CharacterClass getCharacterClass() {
+        return this.characterClass;
+    }
+
+    public void setRace(CharacterRace value) {
+        this.race = value;
+    }
+
+    public CharacterRace getRace() {
+        return this.race;
+    }
+
+    public void setFaction(Faction faction) {
+        this.faction = faction;
+    }
+
+    public Faction getFaction() {
+        return this.faction;
+    }
 
 }
---
+++ src/main/java/de/thomasworm/wowraid/api/model/persistence/Faction.java
@@ -0,0 +1,31 @@
+package de.thomasworm.wowraid.api.model.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity()
+public class Faction {
+
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column()
+    private String name;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+}
