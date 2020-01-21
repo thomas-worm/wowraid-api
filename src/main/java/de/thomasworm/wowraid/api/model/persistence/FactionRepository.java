5544f8b4df7f72580d0caebb69b410783e1187a8
thomas-worm <thomas.worm@thomas-worm.de>
2020-01-21 15:25:10 +0000

    Changed to spring default repository for faction.

--- src/main/java/de/thomasworm/wowraid/api/model/persistence/FactionRepository.java
+++ src/main/java/de/thomasworm/wowraid/api/model/persistence/FactionRepository.java
@@ -1,37 +1,11 @@
 package de.thomasworm.wowraid.api.model.persistence;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
+import org.springframework.data.repository.PagingAndSortingRepository ;
 import org.springframework.stereotype.Repository;
 
 @Repository()
-public class FactionRepository {
+public interface FactionRepository extends PagingAndSortingRepository<Faction, Long> {
 
-    @PersistenceContext()
-    private EntityManager entityManager;
-
-    public Iterable<Faction> findAll() {
-        CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
-        CriteriaQuery<Faction> query = builder.createQuery(Faction.class);
-        Root<Faction> factionTable = query.from(Faction.class);
-        List<Faction> foundFactions = this.entityManager.createQuery(query.select(factionTable)).getResultList();
-        return foundFactions;
-    }
-
-    public Faction findByName(String name) {
-        CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
-        CriteriaQuery<Faction> query = builder.createQuery(Faction.class);
-        Root<Faction> factionTable = query.from(Faction.class);
-        List<Faction> foundFactions = entityManager.createQuery(
-            query.select(factionTable).where(builder.equal(factionTable.get(Faction_.name), name))
-        ).setMaxResults(1).getResultList();
-        return foundFactions.isEmpty() ? null : foundFactions.get(0);
-    }
-
+    public Faction findByName(String name);
+    
 }
