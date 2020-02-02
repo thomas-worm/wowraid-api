08bdba57a01c41cfd03eeb037e0f6a03c2439837
thomas-worm <thomas.worm@thomas-worm.de>
2020-02-02 21:51:37 +0000

    Added REST skeleton for attendee creation.

--- src/main/java/de/thomasworm/wowraid/api/UserService.java
+++ src/main/java/de/thomasworm/wowraid/api/UserService.java
@@ -1,5 +1,8 @@
 package de.thomasworm.wowraid.api;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Service;
@@ -22,6 +25,16 @@ public class UserService {
         user.setBlizzardIdentifier(Integer.parseInt(token.getPrincipal().getName()));
         user.setBattleTag(token.getPrincipal().getAttribute("battle_tag"));
         return this.userRepository.addOrUpdateByBlizzardIdentifier(user);
+    }
+
+    public boolean isUserInAnyGroupWithName(User user, Iterable<String> groups) {
+        Set<String> groupStrings = new HashSet<>();
+        groups.forEach(group -> {
+            if (!groupStrings.contains(group)) {
+                groupStrings.add(group);
+            }   
+        });
+        return user.getGroups().stream().anyMatch(group -> groupStrings.contains(group.getName()));
     }
 
 }
