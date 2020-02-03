188c6b91040b2db88d7f0553baee2623d6a82fdb
thomas-worm <thomas.worm@thomas-worm.de>
2020-02-03 04:18:07 +0000

    Added logic for attendee creation.

--- src/main/java/de/thomasworm/wowraid/api/EventAttendeeService.java
+++ src/main/java/de/thomasworm/wowraid/api/EventAttendeeService.java
@@ -1,6 +1,9 @@
 package de.thomasworm.wowraid.api;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
@@ -13,17 +16,61 @@ import de.thomasworm.wowraid.api.model.persistence.EventAttendee;
 public class EventAttendeeService {
 
     EventAttendee create(Event event, Character character, Iterable<CharacterRole> roles, LocalDateTime startDateTime, LocalDateTime finishDateTime, boolean recursive) {
-        System.out.println("EVENT: " + event.getName());
-        System.out.println("CHARACTER: " + character.getName());
-        StringBuilder rolesStr = new StringBuilder();
-        roles.forEach(role -> {
-            rolesStr.append(" " + role + " ");
+        List<EventAttendee> attendees = new ArrayList<>();
+        buildAttendee(attendees, event, character, roles, startDateTime, finishDateTime, recursive, new HashSet<>());
+        attendees.forEach(attendee -> {
+            System.out.println("EVENT: " + attendee.getEvent().getName());
+            System.out.println("CHARACTER: " + attendee.getCharacter().getName());
+            System.out.println("START: " + attendee.getStartDateTime().toString());
+            System.out.println("FINISH: " + attendee.getFinishDateTime().toString());
         });
-        System.out.println("ROLES: " + rolesStr.toString());
-        System.out.println("START: " + startDateTime.toString());
-        System.out.println("FINISH: " + finishDateTime.toString());
-        System.out.println("RECURSIVE: " + (recursive ? "true" : "false"));
-        return new EventAttendee();
+        return attendees.get(0);
+    }
+
+    private void buildAttendee(List<EventAttendee> attendeeStack, Event event, Character character, Iterable<CharacterRole> roles, LocalDateTime startDateTime, LocalDateTime finishDateTime, boolean recursive, HashSet<String> processedEvents) {
+        EventAttendee attendee = new EventAttendee();
+        attendee.setEvent(event);
+        attendee.setCharacter(character);
+        if (event.getStartDateTime().isBefore(startDateTime)) {
+            if (event.getFinishDateTime().isAfter(startDateTime)) {
+                attendee.setStartDateTime(startDateTime);
+            } else {
+                attendee.setStartDateTime(event.getFinishDateTime());
+            }
+        } else {
+            attendee.setStartDateTime(event.getStartDateTime());
+        }
+        if (event.getFinishDateTime().isAfter(finishDateTime)) {
+            if (event.getStartDateTime().isBefore(finishDateTime)) {
+                attendee.setFinishDateTime(finishDateTime);
+            } else {
+                attendee.setFinishDateTime(event.getStartDateTime());
+            }
+        } else {
+            attendee.setFinishDateTime(event.getFinishDateTime());
+        }
+        roles.forEach(role -> attendee.getRoles().add(role));
+        if (recursive && !processedEvents.contains(event.getKey())) {
+            processedEvents.add(event.getKey());
+            List<Event> childEvents = new ArrayList<>();
+            event.getEventLinks().forEach(eventLink -> {
+                if (eventLink.getType().equals("child")) {
+                    childEvents.add(eventLink.getTarget());
+                }
+            });
+            event.getIncomingEventLinks().forEach(incomingEventLink -> {
+                if (incomingEventLink.getType().equals("parent")) {
+                    childEvents.add(incomingEventLink.getStart());
+                }
+            });
+            childEvents.forEach(child -> {
+                if (!attendee.getFinishDateTime().isBefore(child.getStartDateTime())
+                 && !attendee.getStartDateTime().isAfter(child.getFinishDateTime())) {
+                    buildAttendee(attendeeStack, child, character, roles, startDateTime, finishDateTime, recursive, (HashSet<String>)processedEvents.clone());
+                }
+            });
+        }
+        attendeeStack.add(attendee);
     }
 
 }
